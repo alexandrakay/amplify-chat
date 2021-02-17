85becabdf270ec47588fcc7f99b6018a32ad5ead
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro-2.local>
2021-02-17 01:43:30 -0500

    created amplify, graphql working

---
+++ src/graphql/queries.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getMessage = /* GraphQL */ `
+  query GetMessage($id: ID!) {
+    getMessage(id: $id) {
+      id
+      author
+      body
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listMessages = /* GraphQL */ `
+  query ListMessages(
+    $filter: ModelMessageFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listMessages(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        author
+        body
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
